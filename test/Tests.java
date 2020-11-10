d1468ee0b18133db6bb20baefc6b3d48050716bc
SoeBrand <soe.brandeis@yahoo.de>
2020-11-10 10:24:17 +0100

    Lösung der Aufgabe 1 - 1.2 und 3

--- test/Tests.java
+++ test/Tests.java
@@ -6,7 +6,14 @@ public class Tests {
         System.out.println("Expected: Das Ergebnis der Berechnung: [eins]");
         System.out.print("Returned: "); c.display(1);
         System.out.println("----------------");
-
+        System.out.println("Expected: Das Ergebnis der Berechnung: [Übersetzung der Zahl [0] nicht möglich (1.0)]");
+        System.out.print("Returned: "); c.display(0);
+        System.out.println("----------------");
+        System.out.println("Expected: Das Ergebnis der Berechnung: [Übersetzung der Zahl [-1] nicht möglich (1.0)]");
+        System.out.print("Returned: "); c.display(-1);
+        System.out.println("----------------");
+        System.out.println("Expected: Das Ergebnis der Berechnung: [Übersetzung der Zahl [12] nicht möglich (1.0)]");
+        System.out.print("Returned: "); c.display(12);
     }
 
 }
